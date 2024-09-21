161c9ff3d3c18dd6d654c16911e9bf482e180eb7
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2024-09-21 17:03:33 +0530

    20240921

--- spring/mlm/src/main/java/ols2405/mlm/MemberController.java
+++ spring/mlm/src/main/java/ols2405/mlm/MemberController.java
@@ -1,26 +1,38 @@
 package ols2405.mlm;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+// @RequestMapping(path = "/mlm")
 public class MemberController {
+    @Autowired
     private MemberRepository memberRepository;
 
-    @PostMapping(path="/member")
+    @GetMapping(path = "/members")
+    public String getAllMembers(Map<String, Object> model) {
+        model.put("members", memberRepository.findAll());
+        return "member/member";
+    }
+
+    @GetMapping (path="/member/create")
+    public String getMemberCreate() {
+        return "member/create";
+    }
+
+    @PostMapping(path = "/member")
     public @ResponseBody String addNewMember(@RequestParam String name, @RequestParam String email) {
         Member member = new Member();
         member.setName(name);
         member.setEmail(email);
         memberRepository.save(member);
         return "Saved";
-    }
-
-    @GetMapping(path="/members")
-    public @ResponseBody Iterable<Member> getAllUsers() {
-        return memberRepository.findAll();
     }
 }
---
+++ spring/tacocloud/src/main/java/ols2405/tacocloud/TacocloudApplication.java
@@ -0,0 +1,13 @@
+package ols2405.tacocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacocloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacocloudApplication.class, args);
+	}
+
+}
