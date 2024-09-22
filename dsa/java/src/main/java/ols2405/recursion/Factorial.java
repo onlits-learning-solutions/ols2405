09ea13c5b3bc016ce57ca19a8786fb9d1b5a4e4d
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2024-09-22 17:08:18 +0530

    20240921 - Unit Testing

--- dsa/java/src/main/java/ols2405/recursion/Factorial.java
+++ dsa/java/src/main/java/ols2405/recursion/Factorial.java
@@ -1,5 +1,11 @@
 package ols2405.recursion;
 
 public class Factorial {
-    
+    public static int factorial(int n) {
+        int f = 1;
+        for(int i = n; i>=1; i--) {
+            f *= i;
+        }
+        return f;
+    }
 }
